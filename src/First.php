1c9caf753e8b1fc92a0253c02546d73d592278da
liuda <313273766@qq.com>
2019-09-06 13:07:00 +0800

    Signed-off-by: liuda <313273766@qq.com>

---
+++ src/First.php
@@ -0,0 +1,15 @@
+<?php
+namespace FirstSdk;
+/**
+ * Created by PhpStorm.
+ * User: cy
+ * Date: 2019/9/6
+ * Time: 13:04
+ */
+class FirstClass{
+
+    public function testSdk()
+    {
+        echo 'hello world';
+    }
+}
